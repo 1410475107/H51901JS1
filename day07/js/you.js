2be5886a73c1e30cc22571a3fafa141045332095
1410475107 <1410475107@qq.com>
2019-03-14 11:32:36 +0800

    基础复习和试题讲解

---
+++ day07/js/you.js
@@ -0,0 +1,14 @@
+var a = 10;
+window.onload = function(){
+    Login();
+    Reg();
+}
+function Login(){
+    var usernmame = 'zhanghao';
+    var passwd = 'passwd';
+}
+
+function  Reg(){
+    var usernmame = 'zhanghao';
+    var passwd = 'passwd';
+}
