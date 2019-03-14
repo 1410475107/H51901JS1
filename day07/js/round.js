15e588d869583a4213f04d08655e085913188129
1410475107 <1410475107@qq.com>
2019-03-14 17:31:55 +0800

    上课代码

--- day07/js/round.js
+++ day07/js/round.js
@@ -1,34 +1,82 @@
-(function () {
-    function Round(selector) {
-        // 构造函数里面负责属性的设置和初始函数的执行
-        // 所有的要操作的图片和点
-        this.allimg = document.querySelectorAll(selector + '>ul.img>li');
-        this.allpoint = document.querySelectorAll(selector + '>ul.point>li');
-        // 初始化调用  如果不调用不会执行
+// 防止变量污染  使用函数形成一个封闭的空间
+var h51901={};
+// new h51901.Round('.round1');
+h51901.Round = (function(){
+    // 函数内部的变量是局部变量
+    function Round(selector, speed=1500){
+        // 构造函数完成属性的设置和初始化
+        // 找到所有的“图片”  ====  li
+        this.allimg = document.querySelectorAll(selector+'>ul.img>li');
+        this.allpoint = document.querySelectorAll(selector+'>ul.point>li');
+        this.div = document.querySelector(selector);
+        //获取元素总数
+        this.num =  this.allpoint.length;
+        console.log(this.num);
+        // 定时器的id
+        this.sid = 0;
+        // 是否开启定时器的标识符
+        this.auto = 0;
+        // 当前要显示的图片的下标
+        this.p = 0;
+        // 播放速度
+        this.speed = speed;
+        // 需要初始化
         this.init();
     }
-    //在构造函数的原型之上定义需要的方法
-    Round.prototype.init = function () {
-        console.log(this.allimg);
-        // 把this保留下来，防止下面修改this的指向
+    //功能实现:点击切换图片 事件三要素
+    Round.prototype.init = function(){
+        this.allpoint[0].style.backgroundColor = '#f01';
+        for(var i=0; i < this.allpoint.length; i++){
+            this.bindclick(i);
+        }
+        //鼠标移入整个盒子，清除定时器
         var _this = this;
-        // 循环的方式绑定事件
-        for (var i = 0; i < _this.allpoint.length; i++) {
-            // 点击事件是在最后点击的时候执行的，需要自运行函数提前设置好值()()
-            _this.allpoint[i].onclick = (function (a) {
-                console.log(a);
-                // 点击需要事件处理函数，这里返回一个函数作为事件处理函数
-                return function(){
-                    //隐藏所有的图片
-                    for(var j=0; j<_this.allimg.length;j++){
-                        _this.allimg[j].style.display = 'none';
-                    }
-                    //对应的图片显示出来
-                    _this.allimg[a].style.display = 'block';
-                }
-            })(i);
+        this.div.onmouseover = function(){
+            /*
+            if(_this.sid){
+                clearInterval(_this.sid);
+            }*/
+            _this.sid && clearInterval(_this.sid);
+        }
+        //鼠标移开
+        this.div.onmouseout = function(){
+            if(_this.auto){
+                _this.autoplay(_this.speed);
+            }
         }
     }
-    // 暴露一个接口道全局
-    window.Round = Round;
+    // 根据事件源的索引（下标）绑定事件  0  1  2.。。。。
+    Round.prototype.bindclick = function(p){
+        // 解决this指向被改变的问题
+        var _this = this;
+        this.allpoint[p].onclick = function(){
+            //循环的方式隐藏所有图片
+            _this.allimg;
+            for(var i=0; i < _this.allimg.length; i++){
+                _this.allimg[i].style.display = 'none';
+                //修改点的背景颜色
+                _this.allpoint[i].style.backgroundColor = '';
+            }
+            //显示对应的图片
+            _this.allimg[p].style.display = 'block';
+            // 修改当前点的样式
+            // _this.allpoint[p].style.backgroundColor = '#f01';
+            this.style.backgroundColor = '#f01';
+            //修改当前显示的图片的下标
+            _this.p = p;
+        }
+    }
+
+    // 自动轮播效果  
+    Round.prototype.autoplay = function(){
+        // 开启自动轮播的标识符
+        this.auto = 1;
+        var _this = this;
+        this.sid = setInterval(function(){
+            _this.allpoint[++_this.p%(_this.num)].click();
+        }, _this.speed);
+    }
+    // 暴露一个接口出来
+    // window.Round = Round;
+    return Round;
 })();
